198fed01cda6b04d98fda6ce0a76f5af6778dc02
Marek Auguscik <auguscikma@gmail.com>
2022-11-13 08:17:24 +0100

    Added ability to send generated fills with rand interval

--- fill_server/fill_server.py
+++ fill_server/fill_server.py
@@ -1,6 +1,9 @@
+# pyre-strict
 from abc import abstractmethod
+import http.client
 import json
 import random
+import time
 from typing import List, NamedTuple
 
 
@@ -28,9 +31,39 @@ class StockTickerGenerator:
 class FillServer:
     # api_handler: ControllerAPI
 
+    MAX_SLEEP_TIME: int = 5  # seconds
+    MIN_SLEEP_TIME: int = 1  # seconds
+
     def __init__(self) -> None:
 
         pass
 
     def create_payload(self) -> List[StockTicker]:
         return []
+
+    def processing(self) -> None:
+        try:
+            while True:
+                print("Call")
+                time.sleep(random.randint(self.MIN_SLEEP_TIME, self.MAX_SLEEP_TIME))
+                self.call_controller(
+                    StockTickerGenerator.generate_stock_attributes("RandName")
+                )
+        except KeyboardInterrupt:
+            pass
+
+    def call_controller(self, stock_ticker: StockTicker) -> None:
+        conn = http.client.HTTPConnection("localhost", 8080)
+
+        headers = {"Content-type": "application/json"}
+
+        conn.request("POST", "/fill", stock_ticker.serialize(), headers)
+
+        response = conn.getresponse()
+        print(response.read().decode())
+
+
+if __name__ == "__main__":
+    fille_server_instance = FillServer()
+
+    fille_server_instance.processing()
--- main.py
+++ main.py
@@ -1,36 +1,46 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
 import logging
 import time
+from fill_server.fill_server import StockTicker
 
 hostName = "localhost"
 serverPort = 8080
 
-class MyServer(BaseHTTPRequestHandler):
 
+class MyServer(BaseHTTPRequestHandler):
     def _set_response(self):
         self.send_response(200)
-        self.send_header('Content-type', 'text/html')
+        self.send_header("Content-type", "text/html")
         self.end_headers()
-    
+
     def do_GET(self):
         self._set_response()
-        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+        self.wfile.write("GET request for {}".format(self.path).encode("utf-8"))
 
     def do_POST(self):
-        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-        post_data = self.rfile.read(content_length) # <--- Gets the data itself
-        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
-                str(self.path), str(self.headers), post_data.decode('utf-8'))
+        content_length = int(
+            self.headers["Content-Length"]
+        )  
+        post_data = self.rfile.read(content_length) 
+        logging.info(
+            "POST request,\nPath: %s\n\nBody:\n%s\n",
+            str(self.path),
+            post_data.decode("utf-8"),
+        )
 
         self._set_response()
-        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+        self.post_router(post_data)
+
+    def post_router(self, post_data) -> None:
+        if self.path == "/fill":
+            fill_data = StockTicker(json.loads(post_data))
+            print(fill_data)
+        else:
+            logging.WARNING(f"Unknown path: {self.path}")
 
 
-
-
-
-
-if __name__ == "__main__":        
+if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     webServer = HTTPServer((hostName, serverPort), MyServer)
     print("Server started http://%s:%s" % (hostName, serverPort))
@@ -42,5 +52,3 @@ if __name__ == "__main__":
 
     webServer.server_close()
     print("Server stopped.")
-
-    
