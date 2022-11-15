fe405652767d0907e0a5c703cf57e0ef5551f722
Marek Auguscik <auguscikma@gmail.com>
2022-11-15 09:06:17 +0100

    Add cmdline argument for fill server

--- fill_server/fill_server.py
+++ fill_server/fill_server.py
@@ -1,3 +1,4 @@
+import argparse
 import http.client
 import logging
 import random
@@ -6,7 +7,6 @@ import time
 from urllib.error import HTTPError
 
 import sys
-
 
 # setting path
 sys.path.append("../")
@@ -21,12 +21,17 @@ class FillServer:
     MAX_SLEEP_TIME: int = 5  # seconds
     MIN_SLEEP_TIME: int = 1  # seconds
 
+    stock_name: str
+
+    def __init__(self, name: str):
+        self.stock_name = name
+
     def processing(self) -> None:
         try:
             while True:
                 time.sleep(random.randint(self.MIN_SLEEP_TIME, self.MAX_SLEEP_TIME))
                 self.call_controller(
-                    StockTickerGenerator.generate_stock_attributes("RandName")
+                    StockTickerGenerator.generate_stock_attributes(self.stock_name)
                 )
         except KeyboardInterrupt:
             pass
@@ -48,9 +53,16 @@ class FillServer:
         response = conn.getresponse()
         logging.info(f"Response: {response.read().decode()}")
 
+def cmdline_args_parser():
+    parser = argparse.ArgumentParser(description='Fills server')
+    parser.add_argument('name', type=str,
+                    help='The stocks name for this fill server', default="AXA")
 
+    args=parser.parse_args()
+    return args
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    fill_server_instance = FillServer()
+    args = cmdline_args_parser()
+    fill_server_instance = FillServer(args.name)
 
     fill_server_instance.processing()
